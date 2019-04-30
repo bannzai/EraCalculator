50b1ef370945170c9c1b15696b49303be01edc45
bannzai <kingkong999yhirose@gmail.com>
2019-04-30 21:39:07 +0900

    :sparkles: Add Const

---
+++ Sources/EraCalculator/Const.swift
@@ -0,0 +1,16 @@
+//
+//  Const.swift
+//  EraCalculator
+//
+//  Created by Yudai.Hirose on 2019/04/30.
+//
+
+import Foundation
+
+struct Const {
+    static let second: Int = 1
+    static let minute: Int = second * 60
+    static let hour: Int = minute * 60
+    static let day: Int = hour * 24
+    static let weekday: Int = day * 7
+}
